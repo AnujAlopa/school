a4dec08d22e9162eac679aa4d4a127b546badacf
AnujAlopa <averma@alopasoft.com>
2020-05-24 00:51:32 +0530

    dsf

--- api/EmailService.js
+++ api/EmailService.js
@@ -2,9 +2,9 @@ const EmailService = require('../EmailService/EmailService');
 const envVariable = require("../config/envValues.js");
 const router = require('express').Router();
 
-const add = (a,b) =>{
-  return a+b
-}
+// const add = (a,b) =>{
+//   return a+b
+// }
 //delete Users
 router.post("/contactus", async function (req, res) {
     let mailOptions = {
